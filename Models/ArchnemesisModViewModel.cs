6c5387f9f1a9f7bd04149cb3242a4a11e24b9268
Sean Smith <ranky26@gmail.com>
2022-02-10 12:54:17 +1000

    Added import/export feature for recipes, recipe state is maintained between sessions. Closes #4

--- Models/ArchnemesisModViewModel.cs
+++ Models/ArchnemesisModViewModel.cs
@@ -1,16 +1,24 @@
 ﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
 
 namespace ArchnemesisRecipies.Models
 {
     [DebuggerDisplay("{Name} (T{ModTier})")]
     public class ArchnemesisModViewModel : ArchnemesisMod
     {
+        [JsonIgnore]
         public List<ArchnemesisModViewModel> Components { get; set; }
+        [JsonIgnore]
         public List<ArchnemesisModViewModel> ComponentOf { get; set; }
+        [JsonIgnore]
         public int ModTier => (Components?.Any() ?? false ? Components.Max(x => x.ModTier) : 0) + 1;
+        [JsonIgnore]
         public string HighlightStyle { get; set; }
+        [JsonIgnore]
         public bool Fade { get; set; }
+        [JsonIgnore]
         public bool MouseOver { get; set; }
+        [JsonIgnore]
         public bool Selected { get; set; }
 
         public string GetStyle(string additionalHighlights = "")
