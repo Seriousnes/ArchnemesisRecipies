b4c9f90a629a8d25c6828cea8a3b5443c2e42cc4
Sean Smith <ranky26@gmail.com>
2022-02-11 15:57:36 +1000

    Release

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-// version 1.0.20
+// version 00c769843ac045d993ae2fcf92ff398b
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
